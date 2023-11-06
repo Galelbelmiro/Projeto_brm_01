3ad504bc4598277aaf50989e4de136482e350b9a
Gabriel Belmiro <gbelmiro65@gmail.com>
2023-11-06 09:32:12 -0300

    Executando teste de rota

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// TESTE DE ROTA
+Route::get('testando', function(){
+    return "Esse é um teste de Rota";
+});
